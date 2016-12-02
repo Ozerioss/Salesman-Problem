a7d4357e0cd704b5ea779e21fe8777d2854de1b8
Karim Kidiss <ozerioss@gmail.com>
2016-12-02 11:08:44 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include<limits.h>
 #define pi 3.14159265359
 
 #define R 6371
 
-/*double degToRad(degree)
-{
-    return degree * pi/180;
-}
 
+// Method to calculate the distance between two points defined by latitude and longitude
+// This is also known as the haversine function
+
+double degToRad(double degree)  // Method to convert to Radian from Degree
+{
+    return degree * (pi/180);
+}
 
 double calculer_distance(double lat1,double lon1,double lat2,double lon2)  // On extrait du csv les points latX, lonX
 {
@@ -21,24 +25,13 @@ double calculer_distance(double lat1,double lon1,double lat2,double lon2)  // On
     double d = R * c;  // d distance in km
     return d;
 
-}*/
-// Method to calculate the distance between two points defined by latitude and longitude
-// This is also known as the haversine function
-double calculer_distance_v2(double lat1,double lon1,double lat2,double lon2)  // On extrait du csv les points latX, lonX
-{
-    //static const int R = 6371;  // Radius of the earth
-    double deltaLat = (lat2-lat1) * pi/180; // We multiply by pi/180 to convert the degree to radian
-    double deltaLon = (lon2-lon1) * pi/180;
-    double a = sin(deltaLat/2) * sin(deltaLat/2) + cos((lat1) * pi/180) * cos((lat2) * pi/180) * sin(deltaLon/2) * sin(deltaLon/2); // sin^2(dLat) +sin^2(dLon) * cos(lat1*pi/180) * cos(lat2*pi/180)
-    double c = 2 * atan2(sqrt(a), sqrt(1-a));
-    double d = R * c;  // d distance in km
-    return d;
-
 }
+
+
 
 int main()
 {
-    double x = calculer_distance_v2(41.92706484, 8.728293822, 49.90037661, 2.300004027);
+    double x = calculer_distance(41.92706484, 8.728293822, 49.90037661, 2.300004027);
     printf("La distance est %f", x);
 
 }
