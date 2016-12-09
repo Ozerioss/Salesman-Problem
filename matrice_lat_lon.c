cd57c9f60cd12a0780e38907a1d9a4287a9b585c
Karim Kidiss <ozerioss@gmail.com>
2016-12-09 15:45:22 +0100

    created bruteforce

--- matrice_lat_lon.c
+++ matrice_lat_lon.c
@@ -1,11 +1,13 @@
 
 
+
 #include<limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include<limits.h>
 #include<string.h>
+#include<stdbool.h>
 #define pi 3.14159265359
 
 #define R 6371
@@ -28,26 +30,128 @@ void printCitiesList();
 
 
 //Méthodes pour faciliter le calcul des distances à partir des latitudes / longitudes
-double degToRad(double degree)  // Method to convert to Radian from Degree
+float degToRad(float degree)  // Method to convert to Radian from Degree
 {
     return degree * (pi/180);
 }
 
-double calculer_distance(float lat1,float lon1,float lat2,float lon2)  // On extrait du csv les points latX, lonX
+float calculer_distance(float lat1,float lon1,float lat2,float lon2)  // On extrait du csv les points latX, lonX
 {
     //static const int R = 6371;  // Radius of the earth
 
 
 
-    double deltaLat = degToRad(lat2-lat1); // degToRad is a method that converts the delta of latitudes/longitudes ( which is in degree )
-    double deltaLon = degToRad(lon2-lon1);
-    double a = sin(deltaLat/2) * sin(deltaLat/2) + cos(degToRad(lat1)) * cos(degToRad(lat2)) * sin(deltaLon/2) * sin(deltaLon/2); // sin^2(dLat) +sin^2(dLon) * cos(lat1*pi/180) * cos(lat2*pi/180)
-    double c = 2 * atan2(sqrt(a), sqrt(1-a));
-    double d = R * c; // d distance in km
+    float deltaLat = degToRad(lat2-lat1); // degToRad is a method that converts the delta of latitudes/longitudes ( which is in degree )
+    float deltaLon = degToRad(lon2-lon1);
+    float a = sin(deltaLat/2) * sin(deltaLat/2) + cos(degToRad(lat1)) * cos(degToRad(lat2)) * sin(deltaLon/2) * sin(deltaLon/2); // sin^2(dLat) +sin^2(dLon) * cos(lat1*pi/180) * cos(lat2*pi/180)
+    float c = 2 * atan2(sqrt(a), sqrt(1-a));
+    float d = R * c; // d distance in km
     return d;
 
 
 }
+
+void bruteForce(int a[], int i, int n)
+{
+    int currentCity = 47; // index de Paris qui est notre ville de départ
+    float currentDist = 0;
+    float globalDist = 0;
+
+    int nextCity;   // ID
+    int order[72];
+    order[0] = villes[0].index;
+    int maxGlobalDist = 100000;
+
+    if(i == n)
+    {
+        for(int k=0; k<i+1; k++)
+        {
+
+            nextCity = a[k];
+            order[k+1] = nextCity;
+            currentDist = calculer_distance(villes[k].latitude, villes[k].longitude, villes[k+1].latitude, villes[k].longitude); // on calcule la distance
+            globalDist += currentDist;
+            currentCity = nextCity;
+        }
+
+        if(globalDist>maxGlobalDist)
+        {
+            maxGlobalDist = globalDist;
+            for(int i =0; i<71; i++)
+            {
+
+                printf("ville  %s ->", villes[i].nom);
+            }
+            printf("Paris");
+            maxGlobalDist += calculer_distance(villes[47].latitude, villes[47].longitude, villes[order[i]].latitude, villes[order[i]].longitude);
+        }
+    }
+    else
+    {
+        for(int j=1;j<=n;j++)
+        {
+            permuter(a[i],a[j]);
+            bruteForce(a, i+1, n); // parametre updatés
+            permuter(a[i],a[j]);
+        }
+    }
+}
+
+void permuter(int a, int b)
+{
+    int c = 0;
+    c = a;
+    a = b;
+    b = c;
+}
+
+bool checkVisited(int id, int tab[])
+{
+    bool isVisited;
+    for(int i =0;i<72;i++)
+    {
+        if (tab[i] == id)
+        {
+            isVisited = true;
+        }
+    }
+    return isVisited;
+}
+
+void getLongestPath(float currentDist)
+{
+    int* visited[72];
+    //float currentDist;
+    float globalDist;
+    float maxDist;
+    //struct ville *villes;
+    for(int i = 0;i<72;i++)
+    {
+        if (!checkVisited(villes[i].index,visited[72]))
+        {
+                currentDist = calculer_distance(villes[i].latitude, villes[i].longitude, villes[i+1].latitude, villes[i+1].longitude); // calculate the distance
+                visited[i] = villes[i].index;  // mark it as visited
+                globalDist += currentDist;
+                printf("%f", globalDist);
+        }
+        else
+        {
+            return;
+        }
+
+    }
+
+    for(int j = 0 ; j <= 72 ; j++)
+    {
+        getLongestPath(currentDist + globalDist);
+        printf("%f", globalDist);
+
+    }
+    printf("%f", globalDist);
+
+}
+
+
 
 // Lecture du fichier et sauvegarde dans la liste chainée
 int readCitiesFile()
@@ -105,7 +209,7 @@ void printCitiesList()
     for (i = 1; i <= 71; i++)
     {
         if (villes[i].nom != NULL)
-        printf("index i= %i %s -- %f -- %f\n", villes[i].index, villes[i].nom, villes[i].latitude, villes[i].longitude);
+        printf("index i=%i %s -- %f -- %f\n", villes[i].index, villes[i].nom, villes[i].latitude, villes[i].longitude);
         else
             break;
     }
@@ -115,17 +219,19 @@ void printCitiesList()
 
 int main(int argc, char **argv)
 {
-    int isOK = 0;
 
-    isOK = readCitiesFile();
+    readCitiesFile();
 
     printCitiesList();
-    printf("test\n");
-    //printf("ville : %s", );
+
+   // bruteForce(1,3);
+    //float x = calculer_distance(48.86669293, 2.333335326, 43.61039878, 3.869985716);  // Exemple Paris / Montpellier
 
     system("pause");
     return 0;
 }
+
+
 
 
 
